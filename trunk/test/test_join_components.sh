3765218332f9c7a7a6918251f06e0bea559ac052
mirko.windhoff <mirko.windhoff@f80e46e0-94f1-ba78-e99c-8ca191e85972>
2010-10-01 03:05:51 +0000

    still work in progress, but joining of close or overlapping components works now

--- trunk/test/test_join_components.sh
+++ trunk/test/test_join_components.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-../meshfix sphere1.off sphere2.off -n 2 -j 20 -jc -o join_two_overlapping_spheres_result
-../meshfix close_objects.off -n 4 -j 30 -jc -o join_close_objects_result
+#../meshfix sphere1.off sphere2.off -n 2 -j 20 -jc -o join_two_overlapping_spheres_result
+../meshfix close_objects.off -n 4 -j 10 --no-clean -o join_close_objects_result
 
