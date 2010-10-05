63bac9fff943f7a92c6d7dc4f403cee1c6a0f229
mirko.windhoff <mirko.windhoff@f80e46e0-94f1-ba78-e99c-8ca191e85972>
2010-10-05 15:57:45 +0000

    splitted tests

---
+++ trunk/test/close_objects_join_test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+../meshfix close_objects.off -n 4 -j 10 -jc -o close_objects_join_test_result
+
--- trunk/test/test_join_components.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#../meshfix sphere1.off sphere2.off -n 2 -j 20 -o join_two_overlapping_spheres_result
-../meshfix close_objects.off -n 4 -j 10 -jc -o join_close_objects_result
-
